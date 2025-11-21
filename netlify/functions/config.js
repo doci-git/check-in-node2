e2565df194f7a901c75500ffcb8a17a2c6c998c9
Musa Doci <81824102+doci-git@users.noreply.github.com>
2025-11-21 15:10:22 +0100

    ok2

--- netlify/functions/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  SECRET_KEY: "musart_secret_123_fixed_key",
-  MAX_CLICKS: 3,
-  TIME_LIMIT_MINUTES: 30,
-  BASE_URL_SET: "https://shelly-73-eu.shelly.cloud/v2/devices/api/set/switch",
-};
